cd0574f0c34eddb21a3e0ea536a9babd5ce2749c
Deepak Kumar <48873989+deepakmp444@users.noreply.github.com>
2023-01-20 22:32:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,39 +5,26 @@ function App() {
   const [message, setMessage] = useState("");
   const [getmessage, setGetMessage] = useState("");
 
-   const makeCookies = async (e) => {
-    e.preventDefault();
-    console.log('${process.env.REACT_APP_API}:', process.env.REACT_APP_API)
+  const makeCookies = async (e) => {
 
-    const response = await fetch(`${process.env.REACT_APP_API}message`, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "no-cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "include", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({ message: "message" }), // body data type must match "Content-Type" header
+    // Hint process.env.REACT_APP_API = "REACT_APP_API = "http://localhost:4000"
+    
+    e.preventDefault();
+    const response = await axios.post(`${process.env.REACT_APP_API}/message`, {
+      message,
     });
-    // parses JSON response into native JavaScript objects
+
     console.log("response:", response);
-    // console.log("cookieChecked:", cookieChecked);
   };
 
   const getCookies = async () => {
     try {
-      const getMessageCookie = await axios.get(
-        `${process.env.REACT_APP_API}message`
-      );
+      const getMessageCookie = await axios.get(`${process.env.REACT_APP_API}/message`);
       console.log("getMessageCookie:", getMessageCookie.data.cookieMessage);
     } catch (error) {
       console.log("error:", error);
     }
   };
-
 
   return (
     <div>
